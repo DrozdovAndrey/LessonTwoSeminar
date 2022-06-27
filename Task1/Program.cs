9edb58921d51064b15eced2a05a2cc7e83610cd1
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-27 22:38:43 +0300

    Task 1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld  
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+
+// 782 -> 72
+
+// 918 -> 98
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int number = new Random().Next(100, 1000);
+            Console.WriteLine(number);
+            Console.WriteLine($"{number / 100}{number % 10}");
+            
+        }  
+    }
+}
